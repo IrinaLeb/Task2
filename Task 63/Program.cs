1fd638fddc064658dc1ae2fabe3a93b98b174c17
Irina <gammi_i@mail.ru>
2023-03-02 15:53:39 +0300

    На входе значение N, на выходе натуральные числа в промежутке от 1 до N

---
+++ Task 63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+string NumberFor(int N)
+{
+    if (N == 1) return $"{1}";
+    return NumberFor(N - 1) + $", {N}";
+}
+
+Console.Clear();
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine(NumberFor(N));
