76bd58a3b06e7b0f4944e2a01937f8114b5114b0
Irina <gammi_i@mail.ru>
2023-03-02 16:25:27 +0300

    На входе число, на выходе возвращает сумму его цифр

---
+++ Task 67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDig(int N)
+{
+    if (N == 0) return 0;
+    return N % 10 + SumDig(N / 10);
+}
+
+Console.Clear();
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumDig(N));
